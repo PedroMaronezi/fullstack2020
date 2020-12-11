bc9ea75eeabd413afb65ae5691702fd6e73e07a6
Pedro Maronezi <pedromaronezi@usp.br>
2020-12-11 01:46:07 +0100

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,9 +7,13 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
-const Display = (props) => (
-  <div>{props.name} {props.value}</div>
-)
+const Display = (props) => {
+  if (props.name === 'positive'){
+    return(<div>{props.name} {props.value} %</div>)
+  }
+
+  return(<div>{props.name} {props.value}</div>)
+}
 
 const Header = (props) => <h1>{props.title}</h1>
 
@@ -22,16 +26,27 @@ const App = () => {
   const title = 'give feedback'
   const display = 'statistics'
 
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  const all = good + neutral + bad
+  const average = (good*1 + neutral*0 + bad*(-1))/all
+  const positive = 100*good/all
+
   return (
     <div>
       <Header title={title}></Header>
-      <Button handleClick={() => setGood(good + 1)} text='good'></Button>
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'></Button>
-      <Button handleClick={() => setBad(bad + 1)} text='bad'></Button>
+      <Button handleClick={handleGoodClick} text='good'></Button>
+      <Button handleClick={handleNeutralClick} text='neutral'></Button>
+      <Button handleClick={handleBadClick} text='bad'></Button>
       <Header title={display}></Header>
       <Display name='good' value={good}></Display>
       <Display name='neutral' value={neutral}></Display>
       <Display name='bad' value={bad}></Display>
+      <Display name='all' value={all}></Display>
+      <Display name='average' value={average}></Display>
+      <Display name='positive' value={positive}></Display>
     </div>
   )
 }
